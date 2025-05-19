3f74850c780748e0f3d39bede6e7e6ed7d293775
TiberSeptim1 <v1468029@gmail.com>
2025-05-19 23:32:47 +0530

    completed controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controller/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req,res)=>res.send({'title':'get all users'}));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req,res)=>res.send({'title':'get user details'}));
+userRouter.get('/:id',authorize, getUser);
 
 userRouter.post('/', (req,res)=>res.send({'title':'Create new user'}));
 
